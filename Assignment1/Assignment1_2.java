750da9232702a338b96a7d1f3f4cd7793f3233e8
Aman Ranjan Pani <85098159+Aman-Pani@users.noreply.github.com>
2023-08-20 18:42:49 +0530

    Update Assignment1_2.java

--- Assignment1/Assignment1_2.java
+++ Assignment1/Assignment1_2.java
@@ -1,20 +1,33 @@
-/*Write a java program to demonstrate all arithmetic operators, 
-bitwise operators in a single program.*/ 
-public class Assignment1_2{
+import java.lang.*;
+public class test{
 	public static void main(String[] args){
 		int a = 11,b = 2;
 		System.out.println("num1=80  &  num2=20");
-		System.out.println("num1+num2= " + (a+b));
-		System.out.println("num1-num2= " + (a-b));
-		System.out.println("num1*num2= " + (a*b));
-		System.out.println("num1/num2= " + (a/b));
-		System.out.println("num1%num2= " + (a%b));
+		System.out.println("num1 + num2= " + (a+b));
+		System.out.println("num1 - num2= " + (a-b));
+		System.out.println("num1 * num2= " + (a*b));
+		System.out.println("num1 / num2= " + (a/b));
+		System.out.println("num1 % num2= " + (a%b));
 		
 		System.out.println("num1 & num2= " + (a&b));
 		System.out.println("num1 | num2= " + (a|b));
 		System.out.println("num1 ^ num2= " + (a^b));
 		System.out.println("num1 >>num2= " + (a>>b));
 		System.out.println("num1 <<num2= " + (a<<b));
-		System.out.println("~num1= " + (~a));
+		System.out.println("      ~num1=" + (~a));
 	}
 }
+
+
+// num1=80  &  num2=20
+// num1 + num2= 13
+// num1 - num2= 9
+// num1 * num2= 22
+// num1 / num2= 5
+// num1 % num2= 1
+// num1 & num2= 2
+// num1 | num2= 11
+// num1 ^ num2= 9
+// num1 >>num2= 2
+// num1 <<num2= 44
+//       ~num1=-12
