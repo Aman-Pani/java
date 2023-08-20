4ed9b70c39171b50b66b238bda8c709a3f751dcb
Aman Ranjan Pani <85098159+Aman-Pani@users.noreply.github.com>
2023-08-20 19:39:54 +0530

    Update Assignment1_3.java

--- Assignment1/Assignment1_3.java
+++ Assignment1/Assignment1_3.java
@@ -3,17 +3,19 @@ import java.lang.*;
 public class Assignment1_3{
 	public static void main(String[] args){
 		for(int i=0;i<10;i++)
-			System.out.println("Aman");
+			System.out.println(i + " Aman");
 	}
 }
 
-// Aman
-// Aman
-// Aman
-// Aman
-// Aman
-// Aman
-// Aman
-// Aman
-// Aman
-// Aman
+
+
+// 0 Aman
+// 1 Aman
+// 2 Aman
+// 3 Aman
+// 4 Aman
+// 5 Aman
+// 6 Aman
+// 7 Aman
+// 8 Aman
+// 9 Aman
